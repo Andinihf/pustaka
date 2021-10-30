548a3bcf8651cf58d8d559f2663eeb7cc4dd6aa9
andini <andinihaddainafitri@gmail.com>
2021-10-30 19:05:35 +0700

    Mengerjakan Latihan 1 - 4

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Perkenalkan<br>";
+        echo "Nama saya Andini Hadaina Fitri Mahasiswa Universitas Bina Sarana Informatika<br>";
+        echo "Saya mempunyai hobi mendengarkan musik";
+    }
+    
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Andini</li>
+        <li>Nama Belakang : Hadaina Fitri</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Pesantren Raudhatul Muta'alimin Cilendek</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tasikmalaya</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Bersepeda</li>
+    </ul>
